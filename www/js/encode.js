e31db215820c5463cb16814e904e7191f889fff9
Vincent Seyller <vseyller@laposte.net>
2022-04-26 16:57:38 +0200

    Création formulaire génération

--- www/js/encode.js
+++ www/js/encode.js
@@ -1,3 +1,8 @@
 document.getElementById("retour").addEventListener("click", () => {
     window.location.href = "./index.html";
 });
+
+document.getElementById("form").addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event);
+});
---
+++ www/js/generate.js
@@ -0,0 +1,4 @@
+document.getElementById("retour").addEventListener("click", () => {
+    window.location.href = "./index.html";
+});
+
