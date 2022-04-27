968874ae9e5ca005caf9db4aa1fa2175b2f2f238
DLTBryan <70251144+DLTBryan@users.noreply.github.com>
2022-04-27 13:33:02 +0200

    Added base detection of position markers

--- www/js/decode.js
+++ www/js/decode.js
@@ -1,7 +1,7 @@
 
 
 document.getElementById("retour").addEventListener("click", () => {
-    window.location.href = "./index.html";
+  window.location.href = "./index.html";
 });
 
 document.getElementById("capture").addEventListener("click", () => {
@@ -19,11 +19,16 @@ let video = document.getElementById("video"); //video in html
 let streaming = false;
 let videoCap = null;
 let stream = null;
+
+//mats for qr detection
 let src = null;
+let gryMat = null;
+let blurredGaussMat = null;
+let edgedMat = null;
+let contourMat = null;
+let hierarchie = null;
 
-let dstC1 = null;
-
-let probableMiddles = new Array();
+let dstC1 = null;dstC1
 
 function initVideo(){
   if (streaming) return;
@@ -41,11 +46,16 @@ function initVideo(){
 
   video.addEventListener("canplay", function(ev){
     if (!streaming) {
-      //height = video.videoHeight / (video.videoWidth/width);
-      video.setAttribute("width", width);
-      video.setAttribute("height", height);
+      height = video.videoHeight / (video.videoWidth/width);
+       video.setAttribute("width", width);
+       video.setAttribute("height", height);
       streaming = true;
+      
       videoCap = new cv.VideoCapture(video);
+
+      //video.height = video.videoHeight;
+      //video.width = video.videoWidth;
+      //width = video.videoWidth; height = video.videoHeight;
       
     }
     startVideoProcessing();
@@ -61,17 +71,105 @@ function startVideoProcessing(){
 
 function processVideo(){
   videoCap.read(src);
-  let result = threshImage(src);
+  let result = findQRCode(src);
   cv.imshow("canvasOutput", result);
+
   requestAnimationFrame(processVideo);
 }
 
-function threshImage(src){
-  let mat = new cv.Mat(height, width, cv.CV_8U);
-  cv.cvtColor(src, mat, cv.COLOR_BGR2GRAY);
-  cv.adaptiveThreshold(mat, dstC1, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 51, 0);
-  mat.delete();
-  return dstC1;
+
+
+function findTimingPattern(src){
+
+}
+
+/**
+ * First step of the QR detection.
+ * @param {cv.Mat} src
+ * @return {cv.Mat}
+ * 
+ * Firstly we need to have a greyed Image to input this one in a 
+ * Gaussian Blur to remove as mush Gaussian noise that we can.
+ * 
+ * Applying a Canny function to find the edges of the smoothen 
+ * image. As this image will be used inside a coutour finder Opencv's fucntion.
+ *   
+ * 
+ * 
+ */
+function findQRCode(src){
+
+  initializeMats();
+
+  cv.cvtColor(src, gryMat, cv.COLOR_RGB2GRAY, 0);
+  cv.GaussianBlur(gryMat, blurredGaussMat, new cv.Size(5,5), 0);
+
+  
+  cv.Canny(blurredGaussMat, edgedMat, 100,200);
+
+  cv.findContours(edgedMat, contourMat, hierarchie, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
+  let positionMarkers = findPositionMarkers(ApproxPositionMarkers());
+  
+  deleteMats();
+
+  return src;
+}
+
+function findPositionMarkers(approx){
+  let box  = [];
+  for (let i = 0; i < approx.length; ++i) {
+      let cnt = contourMat.get(approx[i]);
+      let rotatedRect = cv.minAreaRect(cnt);
+      let ratio = rotatedRect.size.width / rotatedRect.size.height
+      if(ratio < 1.4 && ratio > 0.7) {
+          let vertices = cv.RotatedRect.points(rotatedRect);
+          box.push(vertices);
+          for (let j = 0; j < 4; j++)
+            cv.line(src, vertices[j], vertices[(j + 1) % 4], [0, 255, 0, 255], 1, cv.LINE_AA, 0);
+      }
+      cnt.delete();
+  }
+  return box;
+}
+
+function ApproxPositionMarkers(){
+  let j = 0;
+  let count = 0;
+  let markers = new Array();
+
+  for(var i = 0; i < contourMat.size(); i++){
+    j = i;
+    count = 0;
+    while(hierarchie.intPtr(0,j)[2] != -1){
+      j = hierarchie.intPtr(0,j)[2];
+      count++;
+    }
+    
+    if(count >= 5){
+      markers.push(i);
+    }
+  }
+  return markers;
+}
+
+function initializeMats(){
+  gryMat = new cv.Mat(); //greyed image
+  blurredGaussMat = new cv.Mat(); //blurred image
+  edgedMat = new cv.Mat(); //edged image
+  contourMat = new cv.MatVector(); //contour image
+  hierarchie = new cv.Mat(); //output vector
+}
+
+function deleteMats(){
+  gryMat.delete();
+  blurredGaussMat.delete();
+  edgedMat.delete();
+  contourMat.delete();
+  hierarchie.delete();
+}
+
+function getDistanceBetweenPoints(p1, p2){
+  return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
 }
 
 
@@ -82,3 +180,12 @@ function captureCode(){
 function initOpenCV(){
   initVideo();
 }
+
+
+// function threshImage(src){
+//   let mat = new cv.Mat(height, width, cv.CV_8U);
+//   cv.cvtColor(src, mat, cv.COLOR_BGR2GRAY);
+//   cv.adaptiveThreshold(mat, dstC1, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 51, 0);
+//   mat.delete();
+//   return dstC1;
+// }
