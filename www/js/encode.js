1a61a781b2445a20f109add90a747a68b0264a37
Vincent Seyller <vseyller@laposte.net>
2022-04-27 20:32:55 +0200

    Affichage décompte nombre caractères encodage

--- www/js/encode.js
+++ www/js/encode.js
@@ -6,7 +6,12 @@ document.getElementById("retour").addEventListener("click", () => {
 // Récupération de la sauvegarde de l'entrée utilisateur
 if (window.localStorage.getItem("input") != null) {
     document.getElementById("input").value = window.localStorage.getItem("input").toString();
+    document.getElementById("nb-char").innerHTML = document.getElementById("input").value.length;
 }
+
+document.getElementById("input").addEventListener("input", () => {
+    document.getElementById("nb-char").innerHTML = document.getElementById("input").value.length;
+})
 
 // Gestion de la génération de QR Code
 document.getElementById("generate").addEventListener("click", () => {
